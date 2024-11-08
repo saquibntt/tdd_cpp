9a6f1052b9e22d18e1d331697e7fd9c9d5bb5808
Saquib Pandit <saquib.pandit@esolutions.de>
2024-11-08 09:57:52 +0100

    try out exception

---
+++ c5/16_override_setter_withexception/PlaceDescriptionService.cpp
@@ -0,0 +1,78 @@
+#include "PlaceDescriptionService.h"
+#include "Http.h"
+#include "AddressExtractor.h"
+#include "Address.h"
+#include "CurlHttp.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+/// @brief kept code to check some variations///////////
+
+// shared_ptr<Http> PlaceDescriptionService::setHttp()
+// {
+//    std::cout << "************** IN CODE SETHTTP *****************\n" << std::endl;
+//    return make_shared<CurlHttp>();
+    
+// }
+
+// std::shared_ptr<Http> PlaceDescriptionService::checkNull(std::shared_ptr<Http> per)
+// {
+//     if(per == nullptr)
+//     {
+//         throw std::runtime_error("Logic received null persistence object!");
+//     }
+//     return per;
+// }
+
+// PlaceDescriptionService::PlaceDescriptionService(shared_ptr<Http>)
+// : http_{checkNull(setHttp())}
+
+
+PlaceDescriptionService::PlaceDescriptionService(shared_ptr<Http>)
+   : http_{make_shared<CurlHttp>()}
+//...
+
+/////////////////////////////////////////////
+
+string PlaceDescriptionService::summaryDescription(
+      const string& latitude, const string& longitude) const {
+   auto request = createGetRequestUrl(latitude, longitude);
+   auto response = get(request);
+   return summaryDescription(response);
+}
+
+string PlaceDescriptionService::summaryDescription(
+      const string& response) const {
+   AddressExtractor extractor;
+   auto address = extractor.addressFrom(response);
+   return address.summaryDescription();
+}
+
+string PlaceDescriptionService::get(const string& url) const {
+   auto http = gethttpService();
+   http->initialize();
+   return http->get(url);
+}
+
+shared_ptr<Http> PlaceDescriptionService::gethttpService() const {
+   std::cout << "*return the already created instance*\n" << std::endl;
+   return http_;
+}
+
+string PlaceDescriptionService::createGetRequestUrl(
+      const string& latitude, const string& longitude) const {
+   string server{"http://open.mapquestapi.com/"};
+   string document{"nominatim/v1/reverse"};
+   return server + document + "?" +
+          keyValue("format", "json") + "&" +
+          keyValue("lat", latitude) + "&" +
+          keyValue("lon", longitude);
+}
+
+string PlaceDescriptionService::keyValue(
+      const string& key, const string& value) const {
+   return key + "=" + value;
+}
+
